6d607e8e472540586d7e01bd6190a9a0742c632b
Gourob Nandi <gourobnandi28@gmail.com>
2023-01-13 23:38:22 +0530

    change

---
+++ day 1/app.js
@@ -0,0 +1,5 @@
+const heading1 = React.createElement("h1", { id: "h1" }, "Hello Everyone");
+const heading2 = React.createElement("h2", { id: "h2" }, "Hello Everyone from Gora");
+const container = React.createElement("div", { id: "container" }, [ heading1, heading2]); // we are going to use an array
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
