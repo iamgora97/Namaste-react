7d93c143e5d0b9260afd57ada3ec9461ecc2d35c
Gourob Nandi <gourobnandi28@gmail.com>
2023-01-14 15:19:54 +0530

    DAY 3

--- Day 3/app.js
+++ Day 3/app.js
@@ -22,15 +22,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", { id: "h1" }, "Hello Everyone");
-const heading2 = React.createElement(
-  "h2",
-  { id: "h2" },
-  "Hello Everyone from Gora"
+//React.createelement gives an object which converted to html puts in dom
+
+//jsx uses react.createelement then its gets convetred to object then it conveted again to htmldom
+
+// NORMAL VARIBLE
+const heading = (
+  <h1 id="title" key="h2">
+    "Hello Everyone from Gora"
+  </h1>
 );
-const container = React.createElement("div", { id: "container" }, [
-  heading1,
-  heading2,
-]); // we are going to use an array
+// FUNCTIONAL COMPONENT
+const Title = () => (
+  <h1 id="title" key="h2">
+    "Hello Everyone from Gourob"
+  </h1>
+);
+// we are going to use an array
+
+// React Component
+// Functional - New
+const HeaderComponent1 = () => {
+  return (
+    <div>
+      {heading}
+      <Title />
+      <h1>Namaste React Functional Component</h1>
+      <h2>This is a H2 Tag</h2>
+    </div>
+  );
+};
+// Both are same and will work
+const HeaderComponent2 = () => (
+  <div>
+    <h1>Namaste React Functional Component</h1>
+    <h2>This is a H2 Tag</h2>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+// root.render(heading2);// for react element
+root.render(<HeaderComponent1 />); // for react component
+// root.render(<HeaderComponent2 />);
